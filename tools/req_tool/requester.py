e555a85b95ab3d417c15c237d5f0766c38303895
FlexOne開発部 中里　拓哉 <takuya-nakazato@dac.co.jp>
2015-08-27 13:55:03 +0900

    update for vast

--- tools/req_tool/requester.py
+++ tools/req_tool/requester.py
@@ -1,50 +1,78 @@
 # -*- coding: utf-8 -*-
-#usage python requester.py URL roops max_wait_time
+#usage python requester.py URL roop_times max_wait_time outputfile
 
 import sys
 import urllib2
 import random
 import time
+import re
 from datetime import datetime
+from xml.etree.ElementTree import *
 
 argvs = sys.argv
 
-URL = argvs[1]
+# URL = argvs[1]
+URL = "http://xp1.zedo.com/jsc/xp2/fns.vast?n=2696&c=36/2&d=17&s=3&v=vast2&pu=__page-url__&ru=__referrer__&pw=__player-width__&ph=__player-height__&z=__random-number__"
 LOOPS = int(argvs[2])
 TIME = int(argvs[3])
+OUTFILE = argvs[4]
 
-def req(i):
+adid_list = [
+    2346008,
+    2346009,
+    2346010,
+    2346011,
+    2346012
+]
 
-  time.sleep(random.uniform(0,TIME))
-
+def get_res(i):
+  time = get_current_time
   try:
-    res = urllib2.urlopen(URL) 
-    log = "  "+str(i+1)+":\t"+str(res.code)+"\t"+res.msg
-    print log
-  except urllib2.HTTPError, e:    
-    log = "  "+str(i+1)+":\t"+str(e.code)+"\t"+e.reason
-    print log
+    res = urllib2.urlopen(URL)
+    dict = {"res": res.read(), "status": res.code, "msg": res.msg, "time": time}
+  except urllib2.HTTPError, e:
+    dict = {"res": None, "status": e.code, "msg": e.reason, "time": time}
+  return dict
 
-  f.write(log+"\n")
+def get_current_time():
+    current_time = datetime.now().strftime('%Y/%m/%d %H:%M:%S:%f')
+    return current_time
 
+def get_adid(xmlString):
+    elem = fromstring(xmlString)
+    return elem.find(".//Ad").get("id")
 
-def write_now(msg):
-  
-  now = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-  f.write("\n"+msg+":  "+now+"\n\n")
+def check_adid(adid):
+    if not int(adid) in adid_list:
+        print "incorrect adid found!!",
+        return True
+    else: return None
 
-	
 if __name__ == "__main__":
 
-  print "\nRequest", LOOPS, "times.\n"
-  f = open("log.txt","w")
-  write_now("Start");
+  error_lsit = []
+  if re.search( "vast", URL ):  vast = True
+  else:  vast = False
+
+  f = open(OUTFILE,"w")
 
   for i in range(LOOPS):
-    req(i)
-  
-  write_now("End")
+    if i>0:  time.sleep(random.uniform(0,TIME))
+
+    res = get_res(i)
+
+    if vast: adid = get_adid(res["res"])
+    else: adid = ""
+
+    if adid != "":
+        if check_adid(adid): error_lsit.append(str(i))
+
+    log = str(res["status"]) + "," + res["msg"] + "," + adid + "\n"
+    f.write(str(i) + "," + get_current_time() + "," + log)
+    print str(i) + ": " + log,
+
   f.close()
 
   print "\nFinished.\n"
-
+  for num in error_lsit:
+      print num
